f4516a254f360c4bbb503d3ec3281d9f83bff1f6
panjidev-cy <panjibhekti1@gmail.com>
2025-04-24 14:01:22 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+      xs: { max: "479px" }, // Tambahan opsional untuk layar sangat kecil
+    },
     fontFamily: {
       f1: ["Poppins", "sans-serif"],
-      f2: ["Space Mono", "monospace"],
     },
     extend: {
       colors: {
