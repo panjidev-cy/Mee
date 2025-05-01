5ffb6c2dd68a8cbbf05cbd3c544dad9544aaa62a
panjidev-cy <panjibhekti1@gmail.com>
2025-05-01 16:40:57 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,9 @@ import ScrollingCards from "../home/ScrollingCard";
 export default function Home() {
   return (
     <>
-      <div className=" h-auto md:h-screen md:flex md:flex-col md:justify-end md:pb-[30%] md:bg-stone-500">
-        <div className="bg-blue">
-          <Hero></Hero>
-          <Tools></Tools>
-        </div>
+      <div className=" h-auto md:h-screen md:flex md:flex-col md:justify-end md:pb-[40%]">
+        <Hero></Hero>
+        <Tools></Tools>
       </div>
       <ScrollingCards></ScrollingCards>
     </>
